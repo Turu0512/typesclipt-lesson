35941e85afe8a99583dae7abdedde603d206bea5
Turu0512 <hinataptyan@gmail.com>
2022-01-16 20:15:39 +0900

    簡易Todoアプリ完成

--- react-tslesson/src/App.tsx
+++ react-tslesson/src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./components/Todo";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const onAddTodoText = (text: string) => {
+    setTodos((prevTodo) => [
+      ...prevTodo,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={onAddTodoText} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
 
 export default App;
